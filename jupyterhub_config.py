907d8255594600cb9c2fc965b14225505375dcee
Chris Holdgraf <choldgraf@berkeley.edu>
2020-10-12 15:44:22 -0700

    more information about who maintains hubs

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -20,5 +20,17 @@ c.JupyterHub.template_vars = {
         'org_name': 'University of Foo',
         'org_logo': 'https://jupyter.org/assets/nav_logo.svg',
         'org_url': 'https://jupyter.org',
+        'operated_by': {
+            'name': 'Operating Org',
+            'url': 'https://2i2c.org'
+        },
+        'funded_by': {
+            'name': 'Funding Org',
+            'url': 'https://2i2c.org'
+        },
+        'designed_by': {
+            'name': '2i2c',
+            'url': 'https://2i2c.org'
+        }
     }
 }
